6c4e330c15599e9470633395b74e6aa6b84cfac9
YU Zhou <zhouyu@thoughtworks.com>
2020-01-07 14:50:14 +0800

    add find person by name

--- test/tree.spec.js
+++ test/tree.spec.js
@@ -13,3 +13,54 @@ describe('construct a tree', function () {
 		expect(tree.theGreatAncestor).to.be.an.instanceof(Person);
 	})
 });
+
+describe('inquiries on a tree', function () {
+	describe('find person by name', function () {
+			const jsonTree = {
+			name: 'Mike',
+			gender: 'male',
+
+			children: [{
+				name: 'Christina',
+				gender: 'female',
+				children: [{
+					name: 'Yvonne',
+					gender: 'female'
+				}]
+			}, {
+				name: 'Stephan',
+				gender: 'male'
+			}
+			], 
+			spouse: {
+				name: 'Yanni',
+				gender: 'female'
+			}
+		};
+
+		let tree = new Tree(jsonTree);
+		it('should find person info in root by name', function () {
+			let target = tree.findPersonByName('Mike');
+			expect(target).to.not.be.undefined;
+			expect(target.gender).to.equal('male');
+		});
+
+		it('should find person in its children', function () {
+			let target = tree.findPersonByName('Stephan');
+			expect(target).to.not.be.undefined;
+			expect(target.gender).to.equal('male');
+		})
+
+		it('should find person in its spouse', function () {
+			let target = tree.findPersonByName('Yanni');
+			expect(target).to.not.be.undefined;
+			expect(target.gender).to.equal('female');
+		})
+
+		it('should be able to find the person in the third layer', function () {
+			let target = tree.findPersonByName('Yvonne');
+			expect(target).to.not.be.undefined;
+			expect(target.gender).to.equal('female');
+		})
+	});
+});
--- tree.js
+++ tree.js
@@ -4,6 +4,33 @@ class Tree {
 	constructor(jsonObj) {
 		this.theGreatAncestor = new Person(jsonObj);
 	}
+
+	findPersonByName(name) {
+		return this.findPerson(this.theGreatAncestor, name);
+	}
+
+	findPerson(rootPerson, name) {
+		let flattenArray = this.flattenTree(this.theGreatAncestor);
+		return flattenArray.find( p => {
+			return p.name === name;
+		});
+	}
+
+	flattenTree(tree) {
+		if (!tree) {
+			return [];
+		}
+
+		let flattenArray = [];
+
+		flattenArray.push(tree);
+		flattenArray.push(...this.flattenTree(tree.spouse));
+		tree.children.forEach(child =>  {
+			flattenArray.push(...this.flattenTree(child));
+		});
+
+		return flattenArray;
+	}
 }
 
 module.exports = Tree;
